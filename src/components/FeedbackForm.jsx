bd7fb4c89cad35b83fe401d27ca6298c7008d319
Birdperson9 <yorieshkin@gmail.com>
2021-12-17 22:34:12 +0200

    add RatingSelect component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
 import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
   const [btnDisabled, setBtnDisabled] = useState(true)
   const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
     if (text === '') {
-      setBtnDisabled(true)
       setMessage(null)
+      setBtnDisabled(true)
     } else if (text !== '' && text.trim().length <= 10) {
       setMessage('Text must be at least 10 characters')
       setBtnDisabled(true)
@@ -26,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             onChange={handleTextChange}
